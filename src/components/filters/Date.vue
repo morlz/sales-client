8e11b391f07fe39541ea2585f436afc52e95d78b
Королев Аркадий <korolev.a@ladyagroup.ru>
2018-03-06 18:00:42 +0300

    Почти доделан компонент выора даты

--- src/components/filters/Date.vue
+++ src/components/filters/Date.vue
@@ -1,33 +1,41 @@
 <template>
 <q-card class="filterDate">
 	<q-card-title>Фильтр по дате</q-card-title>
-	<q-field label="Параметры поиска">
-		<div class="">
-			123
-		</div>
-	</q-field>
+	<q-card-main>
+		<q-field helper="Условия выбора даты">
+			<ul>
+				<li v-for="item, index in itogCondition" @click="removeCondition(index)">{{ item }}</li>
+			</ul>
+			<p v-if="!itogCondition.length">Нет условий по дате</p>
+		</q-field>
 
-	<q-tabs inverted>
-		<q-tab slot="title" label="Промежуток" name="range"/>
-		<q-tab slot="title" label="1 Значение" name="one"/>
+		<q-tabs inverted v-model="add.tab">
+			<q-tab slot="title" label="Промежуток" name="range"/>
+			<q-tab slot="title" label="1 Значение" name="one"/>
 
-		<q-tab-pane name="range">
-			<q-field helper="С какой даты">
-				<q-datetime v-model="add.range.from" type="datetime" />
-			</q-field>
+			<q-tab-pane name="range">
+				<q-field helper="С какой даты">
+					<q-datetime v-model="add.range.from" type="datetime" />
+				</q-field>
 
-			<q-field helper="По какую дату">
-				<q-datetime v-model="add.range.to" type="datetime" />
-			</q-field>
-		</q-tab-pane>
+				<q-field helper="По какую дату">
+					<q-datetime v-model="add.range.to" type="datetime" />
+				</q-field>
 
-		<q-tab-pane name="one">
+				<q-option-group type="radio" :options="optionsRange" v-model="add.range.type"/>
+			</q-tab-pane>
 
-		</q-tab-pane>
-	</q-tabs>
+			<q-tab-pane name="one" default>
+				<q-option-group type="radio" :options="optionsOne" v-model="add.one.type"/>
+				<q-field helper="Дата">
+					<q-datetime v-model="add.one.date" type="datetime" />
+				</q-field>
+			</q-tab-pane>
+		</q-tabs>
+	</q-card-main>
 
 	<q-card-actions>
-		<q-btn flat color="primary">Добавить</q-btn>
+		<q-btn flat color="primary" @click="addCondition">Добавить</q-btn>
 	</q-card-actions>
 </q-card>
 </template>
@@ -39,17 +47,30 @@ import {
 	mapMutations
 } from 'vuex'
 
-import { QField, QTabs, QTab, QBtn, QCard, QCardTitle, QCardMain, QCardActions, QTabPane, QDatetime } from 'quasar'
+import moment from 'moment'
+
+import { QField, QTabs, QTab, QBtn, QCard, QCardTitle, QCardMain, QCardActions, QTabPane, QDatetime, QOptionGroup } from 'quasar'
+
+const dateTimeFormat = "DD-MM-YYYY HH:mm:ss"
 
 export default {
 	data() {
 		return {
 			add: {
+				tab: "one",
 				range: {
+					conditionType: 'range',
 					from: "",
-					to: ""
+					to: "",
+					type: ""
+				},
+				one: {
+					conditionType: 'one',
+					date: "",
+					type: "="
 				}
-			}
+			},
+			condition: []
 		}
 	},
 	components: {
@@ -62,16 +83,44 @@ export default {
 		QCardMain,
 		QCardActions,
 		QTabPane,
-		QDatetime
+		QDatetime,
+		QOptionGroup
 	},
 	watch: {
 
 	},
 	computed: {
+		optionsOne () {
+			return [
+				{ label: "Равно", value: "=" },
+				{ label: "Не равно", value: "!=" },
+				{ label: "Больше чем", value: ">" },
+				{ label: "Меньше чем", value: "<" },
+			]
+		},
+		optionsRange () {
+			return [
+				{ label: "Входит", value: "" },
+				{ label: "Не входит", value: "!" },
+			]
+		},
+		itogCondition () {
+			return this.condition.map(el => {
+				if (el.conditionType == 'range')
+					return (el.type == '!' ? 'Не в' : 'В') + ` промежутке от ${moment(el.from).format(dateTimeFormat)} до ${moment(el.to).format(dateTimeFormat)}`
 
+				if (el.conditionType == 'one')
+					return this.optionsOne.find(f => f.value == el.type).label + ` ${moment(el.date).format(dateTimeFormat)}`
+			})
+		}
 	},
 	methods: {
-
+		addCondition () {
+			this.condition.push({ ...this.add[this.add.tab] })
+		},
+		removeCondition (index) {
+			this.condition.splice(index, 1)
+		}
 	},
 }
 </script>
